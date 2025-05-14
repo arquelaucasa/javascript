0e6f606086654cf6f9ba1009338decbb2bc28aca
arquelaucasa <arquelaucompra@gmail.com>
2025-05-13 21:43:54 -0300

    aula8.js
    aula8.js

---
+++ aula8.js
@@ -0,0 +1,16 @@
+let x = 10;
+let y = 10; // para comparação sempre três cinal de iqual
+
+console,console.log(x === y); // estamos testando se o valor de "x" é ugual ao valor de "y"
+// as resposta serão true (para verdadeiro/afirmação) false(para falso/negaç~so)
+
+let frutas = "Banana";
+let frutas2 ="maça";
+// aqui estamos perguntando se o valor da variável "frutas" é DIFERENTE da variável "frutas2"
+console.log(frutas !== frutas2);
+
+let numero = 20;
+let segundoNumero = 258;
+//estamos utilizando (maior que >) e (nenor que <) para comprar determinadas informações
+
+console.log(numero >= 30); // ( > ) maior que ( < ) menor que 
