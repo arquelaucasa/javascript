0169dd442c6714d12efb613d21954046109ec62a
arquelaucasa <arquelaucompra@gmail.com>
2025-05-16 19:51:43 -0300

    javascript,-aula9
    javascript,-aula9

--- aula9.js
+++ aula9.js
@@ -10,19 +10,19 @@ const carta = {
 console.log(`meu personagem se chama: ${carta.nome}`);
 
 const loja = {
-    nome: "elétrica",
+    nome: "elétrica N.A",
     interruptor: 31,
     disjuntor: 40,
     slogon: "elétrica N.A LTC"
 };
 
-console.log(`nosso cliente em primeiro lugar: ${loja.nome}`)
+console.log(`nossos cliente em primeiro lugar: ${loja.nome}`);
 
 const carro  = {
-    marca: " ford",
+    marca: " ford fiesta",
     ano: 2003,
     passageiros: 5,
-    modelo: "fiesta herts",
+    modelo: "fiesta herts"
 
 };
-console.log(`é um carro drrvr tanto para trabalha e laser: ${carro,marca}`);
+console.log(`é um carro sever tanto para trabalha e laser: ${carro.marca}`);
