4ef2817879094c1dd9f0c00141dad0e8921dc588
arquelaucasa <arquelaucompra@gmail.com>
2025-05-19 21:45:52 -0300

    aula11
    aula11

---
+++ aula10.js
@@ -0,0 +1,29 @@
+//ARRAY 
+
+const investimentoMensal = [
+    1000, //0 //janeiro // a chave cochete serve para buscar sequencia numeral, sem importar a ordem
+    2000, //1 //fevereiro
+    3000, //2 //março
+    7000, //3 //abril
+    12000 //4 //maio
+];
+console.log(investimentoMensal);
+console.log(`O valor investido no mês de Janeiro foi: ${investimentoMensal[]}`);
+
+
+const investimentoAnual = [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+];
+console.log(investimentoAnual);
+console.log(`O valor investido anual foi: ${investimentoAnual[0]}`);
--- aula9.js
+++ aula9.js
@@ -11,15 +11,16 @@ console.log(`meu personagem se chama: ${carta.nome}`);
 
 const loja = {
     nome: "elétrica N.A",
-    interruptor: 31,
+    interruptor : 31,
     disjuntor: 40,
     slogon: "elétrica N.A LTC"
 };
 
+
 console.log(`nossos cliente em primeiro lugar: ${loja.nome}`);
 
 const carro  = {
-    marca: " ford fiesta",
+    marca: " ford fiesta ",
     ano: 2003,
     passageiros: 5,
     modelo: "fiesta herts"
