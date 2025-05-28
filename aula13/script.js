b7488bc8eb0830d08b46bdde3610de704991d9c0
arquelaucasa <arquelaucompra@gmail.com>
2025-05-27 21:46:20 -0300

    aula 13
    aula 13

---
+++ aula13/script.js
@@ -0,0 +1,15 @@
+// aqui estamamos recuperando noos IG está np html
+let meuElemento = document.getElementById("meuElemento"); 
+
+
+function tornarCirculo() {
+    meuElemento.classList.add("circle"); // adiciono uma class para arredondar o quadrado
+    
+    meuElemento.classList.remove("square"); // removo uma class para  
+}
+
+function tornarQuadrado() {
+    meuElemento.classList.add("square"); //
+
+    meuElemento.classList.remove("circle");
+}
