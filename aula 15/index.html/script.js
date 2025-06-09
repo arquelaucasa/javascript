335435b142d7caa41e7dcd557426fb5972bf113a
arquelaucasa <arquelaucompra@gmail.com>
2025-06-09 20:29:23 -0300

    javacrip
    javacrip

---
+++ aula 15/index.html/script.js
@@ -0,0 +1,22 @@
+//criamaos um objeto chamado "optios" com as configurações para o efeito de gitação
+
+const options = {
+
+    strings: [
+        "bem-vindo ao obra social Dom Bosco",
+        "Desenvolvimento Front-end",
+        "Desenvolvimento Back-end",
+        "Cadastre-se aqui!"
+    ],
+
+
+typeSpeed: 60, // velocidade da digitação (em milisegundos por caractere)
+backSpeed: 40, // velocidade em apagar os texto
+loop: true,    // faz com que as frases se repitam indefinidamente
+backDelay: 1500, // tempo de espera (em milisegundo) antes de começar a apagar uma frase
+smartBacKspace: true // otimiza o apagamento : apaga apenas as caratecteres diferente da proxima frase
+ 
+}
+
+
+new Typed("#typed", options);
